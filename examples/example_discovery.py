a98ddf02620a674958c3d49f5cde4d80a73a34f3
Remi Rampin <remirampin@gmail.com>
2018-10-03 19:46:30 -0400

    Update examples

--- examples/example_discovery.py
+++ examples/example_discovery.py
@@ -1,15 +1,14 @@
 import asyncio
 import logging
-import os
 import time
 
-from datamart_core import SimpleDiscoverer
+from datamart_core import Discoverer
 
 
 logger = logging.getLogger(__name__)
 
 
-class ExampleDiscoverer(SimpleDiscoverer):
+class ExampleDiscoverer(Discoverer):
     """Example discovery plugin.
     """
     data = ('name,country\n'
@@ -22,30 +21,19 @@ class ExampleDiscoverer(SimpleDiscoverer):
         'is_example': True,
     }
 
-    def do_dataset_download(self, dataset_id):
-        """Example download: just write hard-coded CSV to disk.
-        """
-        # Download dataset to storage
-        storage = self.create_shared_storage()
-        with open(os.path.join(storage.path, 'countries.csv'), 'w') as fp:
-            fp.write(self.data)
-
-        # Record it
-        self.dataset_downloaded(dataset_id, storage)
-
     def run(self):
         # State-of-the-art search process
         time.sleep(5)
 
         # We found a dataset
-        dataset_id = self.dataset_found(self.meta)
+        dataset_id = self.record_dataset('storage path here',
+                                         self.meta)
 
-        # Download it
-        self.do_dataset_download(dataset_id)
+        # TODO: Download it
 
     def handle_materialization(self, dataset_id, meta):
         if meta['filename'] == 'nyu.zip':
-            self.do_dataset_download(dataset_id)
+            pass  # TODO
 
 
 if __name__ == '__main__':
