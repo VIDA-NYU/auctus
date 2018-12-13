91898d48f6cc1f4fa9083361836a84682ead9a52
Remi Rampin <remirampin@gmail.com>
2018-12-13 18:25:21 -0500

    Request data for cities rather than stations

--- discovery/noaa/datamart_noaa_discovery/noaa_discovery.py
+++ discovery/noaa/datamart_noaa_discovery/noaa_discovery.py
@@ -22,6 +22,43 @@ def YearIterator(start, end, years):
         yield '%d-01-01' % i, '%d-12-31' % i
 
 
+def get_all(endpoint, delay=0.5, **params):
+    """Handles NOAA API pagination.
+    """
+    headers = {'token': os.environ['NOAA_TOKEN'],
+               'Accept': 'application/json'}
+    results = []
+    failed = 0
+    pages = 0
+    while True:
+        try:
+            r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2' + endpoint,
+                             headers=headers,
+                             params=dict(params,
+                                         limit='1000',
+                                         offset=len(results)))
+            r.raise_for_status()
+        except requests.HTTPError as e:
+            logger.warning("Request failed: %r", e)
+            failed += 1
+            if failed == 10:
+                raise
+            time.sleep(2)
+            continue
+        time.sleep(delay)
+        failed = 0
+        obj = r.json()
+        pages += 1
+        results.extend(obj['results'])
+        resultset = obj['metadata']['resultset']
+        count = int(resultset['count'])
+        if len(results) >= count:
+            break
+
+    logger.info("Downloaded %d pages of data (%d rows)", pages, len(results))
+    return results
+
+
 class NoaaDiscoverer(Discoverer):
     DATASETS = [
         ('GSOM', "Monthly summaries"),
@@ -32,11 +69,11 @@ class NoaaDiscoverer(Discoverer):
         ('AWND', "average wind speed"),
         # ('PRCP', "precipitation"),
     ]
-    STATIONS = [
-        {'station_id': 'GHCND:USW00094789',
-         'station_name': 'JFK INTERNATIONAL AIRPORT, NY US',
-         'latitude': '40.6386', 'longitude': '-73.7622',
-         'city_id': 'CITY:US360019', 'city_name': 'New York, NY US'},
+    CITIES = [
+        {'id': 'CITY:US360019',
+         'name': "New York, NY US",
+         'latitude': 49.230001434521334,
+         'longitude': -74.01151465721249},
     ]
 
     DELAY = 0.5
@@ -47,67 +84,63 @@ class NoaaDiscoverer(Discoverer):
         super(NoaaDiscoverer, self).__init__(*args, **kwargs)
 
         with pkg_resources.resource_stream('datamart_noaa_discovery',
-                                           'noaa_city_stations.csv') as bio:
+                                           'noaa_cities.csv') as bio:
             tio = TextIOWrapper(bio, encoding='utf-8', newline='')
             csvfile = csv.DictReader(tio)
-            assert csvfile.fieldnames == ['station_id', 'station_name',
-                                          'latitude', 'longitude',
-                                          'city_id', 'city_name']
-            self.STATIONS = list(csvfile)
-            for station in self.STATIONS:
-                station['latitude'] = float(station['latitude'])
-                station['longitude'] = float(station['longitude'])
-        logger.info("Loaded %d stations", len(self.STATIONS))
-
-        self.headers = {'token': os.environ['NOAA_TOKEN'],
-                        'Accept': 'application/json'}
+            assert csvfile.fieldnames == ['id', 'name',
+                                          'latitude', 'longitude']
+            self.CITIES = list(csvfile)
+            for city in self.CITIES:
+                city['latitude'] = float(city['latitude'])
+                city['longitude'] = float(city['longitude'])
+        logger.info("Loaded %d cities", len(self.CITIES))
 
     def main_loop(self):
         while True:
             for dataset, dataset_name in self.DATASETS:
-                logger.info("Processing dataset %s (%s)...",
-                            dataset, dataset_name)
                 for datatype, datatype_name in self.DATATYPES:
-                    for station_dict in self.STATIONS:
+                    logger.info("Processing dataset %s (%s), datatype %s "
+                                "(%s)...",
+                                dataset, dataset_name,
+                                datatype, datatype_name)
+                    for city_dict in self.CITIES:
+                        logger.info("Getting city %s (%s)...",
+                                    city_dict['name'], city_dict['id'])
                         try:
                             self.discover_data(dataset, dataset_name,
                                                datatype, datatype_name,
-                                               station_dict)
+                                               city_dict)
                         except Exception:
                             logger.exception("Error handling dataset=%r "
-                                             "datatype=%r station_id=%r",
+                                             "datatype=%r city_id=%r",
                                              dataset, datatype,
-                                             station_dict['station_id'])
+                                             city_dict['id'])
 
             sleep_until = datetime.utcnow() + self.CHECK_INTERVAL
             while datetime.utcnow() < sleep_until:
                 time.sleep((sleep_until - datetime.utcnow()).total_seconds())
 
     def discover_data(self, dataset, dataset_name, datatype, datatype_name,
-                      station_dict):
+                      city_dict):
         # Find available range
         time.sleep(self.DELAY)
-        r = requests.get(
-            'https://www.ncdc.noaa.gov/cdo-web/api/v2/stations/{}'
-                .format(station_dict['station_id']),
-            params=dict(datasetid=dataset,
-                        datatypeid=datatype),
-            headers=self.headers,
-        )
-        r.raise_for_status()
-        station = r.json()
+        stations = get_all('/stations', self.DELAY,
+                           datasetid=dataset, datatypeid=datatype)
+        mindate = min(s['mindate'] for s in stations)
+        maxdate = max(s['maxdate'] for s in stations)
+        logger.info("Available range is %s - %s", mindate, maxdate)
 
         # Download dataset
         # TODO: Record dataset without materializing it?
         self.download_data(dataset, dataset_name,
                            datatype, datatype_name,
-                           station_dict,
-                           station['mindate'], station['maxdate'])
+                           city_dict,
+                           mindate, maxdate)
 
     def download_data(self, dataset, dataset_name, datatype, datatype_name,
-                      station_dict, start, end):
+                      city_dict, start, end):
         logger.info("Downloading data %s (%s) for %s",
-                    datatype, datatype_name, station_dict['station_name'])
+                    datatype, datatype_name, city_dict['name'])
 
         storage = self.create_storage()
         with open(os.path.join(storage.path, 'main.csv'), 'w') as dest:
@@ -117,57 +150,45 @@ class NoaaDiscoverer(Discoverer):
             for r_start, r_end in YearIterator(start, end,
                                                10 if dataset == 'GSOM' else 1):
                 logger.info("%s - %s...", r_start, r_end)
-                offset = 0
-                while True:
-                    time.sleep(self.DELAY)
-                    r = requests.get(
-                        'https://www.ncdc.noaa.gov/cdo-web/api/v2/data',
-                        headers=self.headers,
-                        params=dict(datasetid=dataset,
-                                    datatypeid=datatype,
-                                    stationid=station_dict['station_id'],
-                                    startdate=r_start,
-                                    enddate=r_end,
-                                    limit=1000,
-                                    offset=offset),
-                    )
-                    r.raise_for_status()
-                    obj = r.json()
-                    if 'results' not in obj:
-                        # Not sure what this means. No data?
-                        logger.error("Empty JSON response!")
-                        break
-                    data = obj['results']
-
-                    for row in data:
-                        writer.writerow([row['date'], row['value']])
-
-                    # Check for next page
-                    count = int(obj['metadata']['resultset']['count'])
-                    offset += len(data)
-                    if offset >= count:
-                        break
+                data = get_all('/data', self.DELAY,
+                               datasetid=dataset, datatypeid=datatype,
+                               locationid=city_dict['id'],
+                               startdate=r_start, enddate=r_end)
+                # Sort by date
+                data = sorted(data, key=lambda v: v['date'])
+                # Write the data, one row per date, averaged across stations
+                time = None
+                values = []
+                for row in data:
+                    if row['date'] != time:
+                        if time is not None:
+                            writer.writerow([time, sum(values) / len(values)])
+                        time = row['date']
+                        values = []
+                    values.append(row['value'])
+                if time is not None:
+                    writer.writerow([time, sum(values) / len(values)])
 
         description = "NOAA {} of {} for {}".format(
-            dataset_name, datatype_name, station_dict['station_name']),
+            dataset_name, datatype_name, city_dict['name']),
 
         self.record_dataset(
             storage,
             dict(
                 noaa_dataset_id=dataset,
                 noaa_datatype_id=datatype,
-                noaa_station_id=station_dict['station_id'],
+                noaa_station_id=city_dict['id'],
                 noaa_start=start,
                 noaa_end=end,
             ),
             dict(
                 name=description,
                 description=description,
-                latitude=station_dict['latitude'],
-                longitude=station_dict['longitude'],
+                latitude=city_dict['latitude'],
+                longitude=city_dict['longitude'],
             ),
             dataset_id='{}.{}.{}'.format(dataset, datatype,
-                                         station_dict['station_id']))
+                                         city_dict['id']))
 
 
 if __name__ == '__main__':
