fc7d47c90b89d4da16637428b83d25c28e880c18
Remi Rampin <r@remirampin.com>
2019-05-24 19:09:39 -0400

    Optimize Docker build

---
+++ install_deps.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""Install dependencies from Pipfile.lock
+
+This scripts is used as part of the Docker build to install all dependencies as
+an initial step before building the images. This makes caching efficient,
+allowing for faster builds that work offline.
+
+It means all images have all dependencies installed, but thanks to
+de-duplication, this generally uses less space if all images exist on the same
+machine.
+"""
+
+import json
+import subprocess
+import sys
+
+
+def main():
+    with open(sys.argv[1]) as fp:
+        lockfile = json.load(fp)
+
+    packages = []
+
+    for name, dep in lockfile['default'].items():
+        if 'path' in dep:
+            continue
+        packages.append('%s=%s' % (name, dep['version']))
+
+    subprocess.check_call(['pip3', 'install'] + packages)
+
+
+if __name__ == '__main__':
+    main()
