e235e615898a4c0def675654532aeda69d7d859a
Remi Rampin <r@remirampin.com>
2019-05-29 14:37:47 +1000

    Add Kubernetes config

---
+++ scripts/minikube-load-images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+if [ "x${DOCKER_HOST-}" = x ]; then
+    echo "DOCKER_HOST is not set; you should run 'eval \$(minikube docker-env)" >&2
+    exit 1
+fi
+
+for image in busybox docker.elastic.co/elasticsearch/elasticsearch:6.4.3 remram/rabbitmq:3.7.8 datamart_coordinator datamart_query datamart_profiler datamart_example_discoverer; do
+    echo "Loading image $image..."
+    DOCKER_HOST= sudo -g docker docker save $image | docker load
+done
