b382f751c6feda438f6dc8611a464a2deab45fbe
Remi Rampin <r@remirampin.com>
2019-07-26 15:59:09 -0400

    Get rid of all-static FilesystemLocks class

--- lib_core/datamart_core/fscache.py
+++ lib_core/datamart_core/fscache.py
@@ -32,111 +32,108 @@ def shell_escape(s):
 WAIT_FOREVER_COMMAND = 'while true; do sleep 3600; done'
 
 
-class FilesystemLocks(object):
-    """File locking system.
+def lock_exclusive(filepath, timeout=None):
+    """Get an exclusive lock.
+
+    The file is created if it doesn't exist.
     """
-    def lock_exclusive(self, filepath, timeout=None):
-        """Get an exclusive lock.
+    args = ['-x']
+    if timeout is not None:
+        args.extend(['--wait', timeout, '-E', 2])
+    args.extend([
+        filepath, '-c',
+        'echo LOCKED; %s' % WAIT_FOREVER_COMMAND,
+    ])
+    proc = subprocess.Popen(
+        ['flock'] + args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        stdin=subprocess.PIPE,
+        # Causes subprocesses (e.g. sleep) to get killed when the shell
+        # process group is killed
+        start_new_session=True,
+    )
+    proc.stdin.close()
 
-        The file is created if it doesn't exist.
-        """
-        args = ['-x']
-        if timeout is not None:
-            args.extend(['--wait', timeout, '-E', 2])
-        args.extend([
-            filepath, '-c',
-            'echo LOCKED; %s' % WAIT_FOREVER_COMMAND,
-        ])
-        proc = subprocess.Popen(
-            ['flock'] + args,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            stdin=subprocess.PIPE,
-            # Causes subprocesses (e.g. sleep) to get killed when the shell
-            # process group is killed
-            start_new_session=True,
-        )
-        proc.stdin.close()
-
-        # Read out of stdout
-        out = proc.stdout.read(6)
-        if out == b'LOCKED':
-            logger.info("Acquired exclusive lock: %r", filepath)
-            return proc, filepath
+    # Read out of stdout
+    out = proc.stdout.read(6)
+    if out == b'LOCKED':
+        logger.info("Acquired exclusive lock: %r", filepath)
+        return proc, filepath
+    else:
+        out = proc.stdout.read()
+        if proc.wait() == 2:
+            logger.debug("Timeout getting exclusive lock: %r", filepath)
+            return None
         else:
-            out = proc.stdout.read()
-            if proc.wait() == 2:
-                logger.debug("Timeout getting exclusive lock: %r", filepath)
-                return None
-            else:
-                raise OSError("Error getting exclusive lock %r: %s" % (
-                    filepath,
-                    out.decode('utf-8', 'replace'),
-                ))
+            raise OSError("Error getting exclusive lock %r: %s" % (
+                filepath,
+                out.decode('utf-8', 'replace'),
+            ))
 
-    def unlock_exclusive(self, lock):
-        proc, filepath = lock
-        logger.debug("Releasing exclusive lock: %r", filepath)
-        os.killpg(proc.pid, signal.SIGINT)
-        try:
-            proc.wait(10)
-        except subprocess.TimeoutExpired:
-            logger.critical("Failed to release exclusive lock: %r", filepath)
-            raise SystemExit("Failed to release exclusive lock: %r" % filepath)
-        logger.info("Released exclusive lock: %r", filepath)
 
-    def lock_shared(self, filepath, timeout=None):
-        """Get a shared lock.
+def unlock_exclusive(lock):
+    proc, filepath = lock
+    logger.debug("Releasing exclusive lock: %r", filepath)
+    os.killpg(proc.pid, signal.SIGINT)
+    try:
+        proc.wait(10)
+    except subprocess.TimeoutExpired:
+        logger.critical("Failed to release exclusive lock: %r", filepath)
+        raise SystemExit("Failed to release exclusive lock: %r" % filepath)
+    logger.info("Released exclusive lock: %r", filepath)
 
-        :raises FileNotFoundError: if the file doesn't exist.
-        """
-        cmd = '(flock -s %s 3 && echo LOCKED && %s) 3<%s' % (
-            '--wait %d -E 2' % timeout if timeout is not None else '',
-            WAIT_FOREVER_COMMAND,
-            shell_escape(filepath),
-        )
-        proc = subprocess.Popen(
-            ['bash', '-c', cmd],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            stdin=subprocess.PIPE,
-            # Causes subprocesses (e.g. sleep) to get killed when the shell
-            # process group is killed
-            start_new_session=True,
-        )
-        proc.stdin.close()
 
-        # Read out of stdout
-        out = proc.stdout.read(6)
-        if out == b'LOCKED':
-            logger.info("Acquired shared lock: %r", filepath)
-            return proc, filepath
+def lock_shared(filepath, timeout=None):
+    """Get a shared lock.
+
+    :raises FileNotFoundError: if the file doesn't exist.
+    """
+    cmd = '(flock -s %s 3 && echo LOCKED && %s) 3<%s' % (
+        '--wait %d -E 2' % timeout if timeout is not None else '',
+        WAIT_FOREVER_COMMAND,
+        shell_escape(filepath),
+    )
+    proc = subprocess.Popen(
+        ['bash', '-c', cmd],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        stdin=subprocess.PIPE,
+        # Causes subprocesses (e.g. sleep) to get killed when the shell
+        # process group is killed
+        start_new_session=True,
+    )
+    proc.stdin.close()
+
+    # Read out of stdout
+    out = proc.stdout.read(6)
+    if out == b'LOCKED':
+        logger.info("Acquired shared lock: %r", filepath)
+        return proc, filepath
+    else:
+        out = proc.stdout.read()
+        if proc.wait() == 2:
+            logger.debug("Timeout getting shared lock: %r", filepath)
+            return None
+        elif proc.wait() == 1:
+            raise FileNotFoundError
         else:
-            out = proc.stdout.read()
-            if proc.wait() == 2:
-                logger.debug("Timeout getting shared lock: %r", filepath)
-                return None
-            elif proc.wait() == 1:
-                raise FileNotFoundError
-            else:
-                raise OSError("Error getting shared lock %r: %s" % (
-                    filepath,
-                    out.decode('utf-8', 'replace'),
-                ))
-
-    def unlock_shared(self, lock):
-        proc, filepath = lock
-        logger.debug("Releasing shared lock: %r", filepath)
-        os.killpg(proc.pid, signal.SIGINT)
-        try:
-            proc.wait(10)
-        except subprocess.TimeoutExpired:
-            logger.critical("Failed to release shared lock: %r", filepath)
-            raise SystemExit("Failed to release shared lock: %r" % filepath)
-        logger.info("Released shared lock: %r", filepath)
+            raise OSError("Error getting shared lock %r: %s" % (
+                filepath,
+                out.decode('utf-8', 'replace'),
+            ))
 
 
-FilesystemLocks = FilesystemLocks()
+def unlock_shared(lock):
+    proc, filepath = lock
+    logger.debug("Releasing shared lock: %r", filepath)
+    os.killpg(proc.pid, signal.SIGINT)
+    try:
+        proc.wait(10)
+    except subprocess.TimeoutExpired:
+        logger.critical("Failed to release shared lock: %r", filepath)
+        raise SystemExit("Failed to release shared lock: %r" % filepath)
+    logger.info("Released shared lock: %r", filepath)
 
 
 @contextlib.contextmanager
@@ -162,7 +159,7 @@ def cache_get_or_set(path, create_function):
     lock_path = path + '.lock'
     while True:
         try:
-            lock = FilesystemLocks.lock_shared(lock_path)
+            lock = lock_shared(lock_path)
         except FileNotFoundError:
             pass
         else:
@@ -173,9 +170,9 @@ def cache_get_or_set(path, create_function):
                     return
                 # Entry was removed while we waited -- we'll try creating
             finally:
-                FilesystemLocks.unlock_shared(lock)
+                unlock_shared(lock)
 
-        lock = FilesystemLocks.lock_exclusive(lock_path)
+        lock = lock_exclusive(lock_path)
         try:
             if os.path.exists(path):
                 # Cache was created while we waited
@@ -188,7 +185,7 @@ def cache_get_or_set(path, create_function):
                 # We can't downgrade to a shared lock, so restart
                 continue
         finally:
-            FilesystemLocks.unlock_exclusive(lock)
+            unlock_exclusive(lock)
 
 
 def clear_cache(cache_root, should_delete=None):
@@ -213,7 +210,7 @@ def clear_cache(cache_root, should_delete=None):
             continue
         lock_path = path + '.lock'
         logger.info("Locking entry: %r", fname)
-        lock = FilesystemLocks.lock_exclusive(lock_path, timeout=300)
+        lock = lock_exclusive(lock_path, timeout=300)
         if lock is None:
             logger.warning("Entry is locked: %r", fname)
             continue
@@ -225,4 +222,4 @@ def clear_cache(cache_root, should_delete=None):
             else:
                 logger.error("Concurrent deletion?! Entry is gone: %r", fname)
         finally:
-            FilesystemLocks.unlock_exclusive(lock)
+            unlock_exclusive(lock)
