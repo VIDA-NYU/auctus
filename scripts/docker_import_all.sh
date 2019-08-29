58600f37af9264813f01e13bb5c55db0377c2146
Remi Rampin <r@remirampin.com>
2019-08-29 16:04:14 -0400

    Fix import script to overwrite

--- scripts/docker_import_all.sh
+++ scripts/docker_import_all.sh
@@ -3,4 +3,4 @@ envfile="$(dirname "$(dirname "$0")")/.env"
 if [ -e "$envfile" ]; then
     . "$envfile"
 fi
-docker run -ti --rm --network datamart_default -v $PWD/scripts:/scripts -v /home/ubuntu/index.20190114:/index -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e AMQP_HOST=rabbitmq -e AMQP_USER=$AMQP_USER -e AMQP_PASSWORD=$AMQP_PASSWORD datamart_coordinator python /scripts/import_all.py /index
+docker run -ti --rm --network datamart_default -v $PWD/scripts:/scripts -v /home/ubuntu/index.20190114:/index -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e AMQP_HOST=rabbitmq -e AMQP_USER=$AMQP_USER -e AMQP_PASSWORD=$AMQP_PASSWORD -e LAZO_SERVER_HOST=lazo_server -e LAZO_SERVER_PORT=50051 datamart_coordinator python /scripts/import_all.py /index
--- scripts/import_all.py
+++ scripts/import_all.py
@@ -3,13 +3,15 @@ import aio_pika
 import asyncio
 import elasticsearch
 import json
+import lazo_index_service
 import logging
 import os
 import sys
 import time
 
 from datamart_core.common import add_dataset_to_index, \
-    add_dataset_to_lazo_storage, json2msg, decode_dataset_id
+    delete_dataset_from_index, add_dataset_to_lazo_storage, json2msg, \
+    decode_dataset_id
 
 
 async def import_all(folder):
@@ -26,31 +28,52 @@ async def import_all(folder):
         'datasets',
         aio_pika.ExchangeType.TOPIC,
     )
+    if 'LAZO_SERVER_HOST' in os.environ:
+        lazo_client = lazo_index_service.LazoIndexClient(
+            host=os.environ['LAZO_SERVER_HOST'],
+            port=int(os.environ['LAZO_SERVER_PORT'])
+        )
+    else:
+        lazo_client = None
 
+    print("Importing Elasticsearch data")
     for name in os.listdir(folder):
+        if name.startswith('lazo.'):
+            continue
         path = os.path.join(folder, name)
         with open(path, 'r') as fp:
             obj = json.load(fp)
-        if name.startswith('lazo.'):
-            id = decode_dataset_id(name[5:])
-            try:
-                add_dataset_to_lazo_storage(es, id, obj)
-            except elasticsearch.TransportError:
-                print('X', end='', flush=True)
-                time.sleep(10)  # If writing can't keep up, needs a real break
-                add_dataset_to_lazo_storage(es, id, obj)
-        else:
-            dataset_id = decode_dataset_id(name)
-            try:
-                add_dataset_to_index(es, dataset_id, obj)
-            except elasticsearch.TransportError:
-                print('X', end='', flush=True)
-                time.sleep(10)  # If writing can't keep up, needs a real break
-                add_dataset_to_index(es, dataset_id, obj)
-            await amqp_datasets_exchange.publish(
-                json2msg(dict(obj, id=dataset_id)),
-                dataset_id,
-            )
+
+        dataset_id = decode_dataset_id(name)
+        try:
+            delete_dataset_from_index(es, dataset_id, lazo_client)
+            add_dataset_to_index(es, dataset_id, obj)
+        except elasticsearch.TransportError:
+            print('X', end='', flush=True)
+            time.sleep(10)  # If writing can't keep up, needs a real break
+            delete_dataset_from_index(es, dataset_id, lazo_client)
+            add_dataset_to_index(es, dataset_id, obj)
+        await amqp_datasets_exchange.publish(
+            json2msg(dict(obj, id=dataset_id)),
+            dataset_id,
+        )
+        print('.', end='', flush=True)
+
+    print("Importing Lazo data")
+    for name in os.listdir(folder):
+        if not name.startswith('lazo.'):
+            continue
+        path = os.path.join(folder, name)
+        with open(path, 'r') as fp:
+            obj = json.load(fp)
+
+        id = decode_dataset_id(name[5:])
+        try:
+            add_dataset_to_lazo_storage(es, id, obj)
+        except elasticsearch.TransportError:
+            print('X', end='', flush=True)
+            time.sleep(10)  # If writing can't keep up, needs a real break
+            add_dataset_to_lazo_storage(es, id, obj)
         print('.', end='', flush=True)
 
 
