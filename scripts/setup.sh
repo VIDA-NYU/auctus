7838295a5299a4bc42518e9716f0561428cb04b5
Remi Rampin <r@remirampin.com>
2019-09-30 15:11:00 -0400

    Update setup.sh script to set up volumes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
+set -eux
+
+sudo(){
+  if [ $(id -u) = 0 ]; then
+    "$@"
+  else
+    command sudo "$@"
+  fi
+}
+
 # Build the SCDP jar
-docker-compose build profiler
-docker run --rm -i datamart_profiler cat /usr/src/app/lib_profiler/datamart_profiler/scdp.jar \
-    >"$(dirname "$0")/../lib_profiler/datamart_profiler/scdp.jar"
+#docker-compose build profiler
+#docker run --rm -i datamart_profiler cat /usr/src/app/lib_profiler/datamart_profiler/scdp.jar \
+#    >"$(dirname "$0")/../lib_profiler/datamart_profiler/scdp.jar"
+
+# Set up volume permissions
+mkdir -p volumes/prometheus && sudo chown -R 65534:65534 volumes/prometheus
+mkdir -p volumes/elasticsearch && sudo chown -R 1000:0 volumes/elasticsearch
+mkdir -p volumes/grafana && sudo chown -R 427:472 volumes/grafana
