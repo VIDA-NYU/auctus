49df606be782307fd5ebb487daee6d7fddbb67a9
Remi Rampin <r@remirampin.com>
2019-10-02 16:31:29 -0400

    Remove loading envfile from scripts

--- scripts/docker_clear_caches.sh
+++ scripts/docker_clear_caches.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-envfile="$(dirname "$(dirname "$0")")/.env"
-if [ -e "$envfile" ]; then
-    . "$envfile"
-fi
 docker run -ti --rm --network datamart_default -v $PWD/scripts:/scripts datamart_coordinator python /scripts/clear_caches.py
