246b8a8b4115bc3851eea5f87c2c5eb6321a1773
Remi Rampin <r@remirampin.com>
2019-10-02 23:00:17 -0400

    Add an argument to import/export scripts

--- scripts/docker_export_all.sh
+++ scripts/docker_export_all.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$(dirname "$0")")"
 PROJ="$(basename "$(pwd)")"
-docker run -ti --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v /home/ubuntu/index.20190114:/index -w /index -e ELASTICSEARCH_HOSTS=elasticsearch:9200 ${PROJ}_coordinator python /scripts/export_all.py
+if [ -z "$1" ]; then
+    echo "Missing argument" >&2
+    exit 1
+fi
+docker run --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v "$1:/index" -w /index -e ELASTICSEARCH_HOSTS=elasticsearch:9200 ${PROJ}_coordinator python /scripts/export_all.py
--- scripts/docker_import_all.sh
+++ scripts/docker_import_all.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$(dirname "$0")")"
 PROJ="$(basename "$(pwd)")"
-docker run -ti --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v /home/ubuntu/index.20190114:/index -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e AMQP_HOST=rabbitmq -e AMQP_USER=$AMQP_USER -e AMQP_PASSWORD=$AMQP_PASSWORD -e LAZO_SERVER_HOST=lazo_server -e LAZO_SERVER_PORT=50051 ${PROJ}_coordinator python /scripts/import_all.py /index
+if [ -z "$1" ]; then
+    echo "Missing argument" >&2
+    exit 1
+fi
+docker run -ti --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v "$1:/index" -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e AMQP_HOST=rabbitmq -e AMQP_USER=$AMQP_USER -e AMQP_PASSWORD=$AMQP_PASSWORD -e LAZO_SERVER_HOST=lazo_server -e LAZO_SERVER_PORT=50051 ${PROJ}_coordinator python /scripts/import_all.py /index
