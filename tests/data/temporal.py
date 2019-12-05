95f608086d4fa831add791df184698fa6026b48a
Remi Rampin <r@remirampin.com>
2019-12-05 09:26:58 -0500

    Add temporal test data

---
+++ tests/data/temporal.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+import os
+import random
+
+
+def main():
+    data_dir = os.path.dirname(__file__)
+
+    with open(os.path.join(data_dir, 'daily.csv'), 'w') as f_daily:
+        print('aug_date,rain', file=f_daily)
+        date = datetime(2019, 4, 23)
+        rand = random.Random(1)
+        for _ in range(30):
+            time = date.date().isoformat()
+            boolean = ['no', 'yes'][rand.randint(0, 1)]
+            print('%s,%s' % (time, boolean), file=f_daily)
+            date += timedelta(days=1)
+
+    with open(os.path.join(data_dir, 'hourly.csv'), 'w') as f_hourly:
+        print('aug_date,rain', file=f_hourly)
+        date = datetime(2019, 6, 12)
+        rand = random.Random(2)
+        for _ in range(52):
+            time = date.isoformat()
+            boolean = ['no', 'yes'][rand.randint(0, 1)]
+            print('%s,%s' % (time, boolean), file=f_hourly)
+            date += timedelta(hours=1)
+
+
+if __name__ == '__main__':
+    main()
