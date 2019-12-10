8b0ffbc78242e06dc63a948e312e0badc27a83e9
Remi Rampin <r@remirampin.com>
2019-12-09 21:07:04 -0500

    Rename 'lazo_server' container to 'lazo'

--- scripts/docker_import_all.sh
+++ scripts/docker_import_all.sh
@@ -5,4 +5,4 @@ if [ -z "$1" ]; then
     echo "Missing argument" >&2
     exit 1
 fi
-docker run -ti --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v "$1:/index" -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e AMQP_HOST=rabbitmq -e AMQP_USER=$AMQP_USER -e AMQP_PASSWORD=$AMQP_PASSWORD -e LAZO_SERVER_HOST=lazo_server -e LAZO_SERVER_PORT=50051 ${PROJ}_coordinator python /scripts/import_all.py /index
+docker run -ti --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v "$1:/index" -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e AMQP_HOST=rabbitmq -e AMQP_USER=$AMQP_USER -e AMQP_PASSWORD=$AMQP_PASSWORD -e LAZO_SERVER_HOST=lazo -e LAZO_SERVER_PORT=50051 ${PROJ}_coordinator python /scripts/import_all.py /index
--- scripts/docker_purge_source.sh
+++ scripts/docker_purge_source.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$(dirname "$0")")"
 PROJ="$(basename "$(pwd)")"
-docker run -ti --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v $PWD/volumes/cache:/cache -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e LAZO_SERVER_HOST=lazo_server -e LAZO_SERVER_PORT=50051 ${PROJ}_coordinator python /scripts/purge_source.py "$1"
+docker run -ti --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v $PWD/volumes/cache:/cache -e ELASTICSEARCH_HOSTS=elasticsearch:9200 -e LAZO_SERVER_HOST=lazo -e LAZO_SERVER_PORT=50051 ${PROJ}_coordinator python /scripts/purge_source.py "$1"
