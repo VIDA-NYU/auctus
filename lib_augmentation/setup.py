711de6dbce9d40a081d49113b108373c1f6d8a6e
Remi Rampin <r@remirampin.com>
2020-02-02 08:54:12 -0500

    Bump datamart-augmentation to 0.5.4

--- lib_augmentation/setup.py
+++ lib_augmentation/setup.py
@@ -9,12 +9,12 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 req = [
     'pandas',
     'numpy',
-    'datamart_materialize==0.5.2',
+    'datamart_materialize==0.5.4',
 ]
 with io.open('README.rst', encoding='utf-8') as fp:
     description = fp.read()
 setup(name='datamart_augmentation',
-      version='0.5.2',
+      version='0.5.4',
       packages=['datamart_augmentation'],
       install_requires=req,
       description="Augmentation functions for Datamart services",
