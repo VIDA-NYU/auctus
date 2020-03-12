c61e17469f913e1061f0fe7da4edc9e12fbc5186
Aecio Santos <aecio.solando@gmail.com>
2020-03-12 00:42:38 -0400

    Fix typos

--- frontend/src/components/visus/PersistentComponent/PersistentComponent.tsx
+++ frontend/src/components/visus/PersistentComponent/PersistentComponent.tsx
@@ -6,11 +6,11 @@ const cache = new Map<string, {}>();
 
 /**
  * This component uses the key provided to a component to generate a cache key for the data.
- * We chose to use key for the follwoing reasons:
+ * We chose to use key for the following reasons:
  * 1. React uses key to identify if the element associated with the component.
- *    In somes cases this helps it to identify that two instance are the same, and avoid re-constructing the instance.
- *    It is expected that this strategy will help react to avoid destroying a component unecessarly.
- * 2. React does some work to avoid siblings with the same key, This shouldprovide some warnings when reusing a key.
+ *    In some cases this helps it to identify that two instance are the same, and avoid re-constructing the instance.
+ *    It is expected that this strategy will help react to avoid destroying a component unnecessarily.
+ * 2. React does some work to avoid siblings with the same key, This should provide some warnings when reusing a key.
  * 3. Since it is an internal from each component, it doesn't pollute the props of components.
  *
  */
@@ -18,7 +18,7 @@ export default class PersistentComponent<TProps = {}, TState = {}> extends PureC
   componentDidMount() {
     // @ts-ignore
     if (!this._reactInternalFiber.key) {
-      console.warn('When using PersistendCompoent please provide the key prop');
+      console.warn('When using PersistentComponent please provide the key prop');
     }
     const cacheKey = this.getCacheKey();
     const previousState = cache.get(cacheKey);
