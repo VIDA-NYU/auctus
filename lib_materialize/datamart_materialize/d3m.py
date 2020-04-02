38e76f5d951ef6cfe1907e4fde0ee9b99252e033
Remi Rampin <r@remirampin.com>
2020-04-02 18:06:48 -0400

    Add an option to D3mWriter to generate d3mIndex

--- lib_materialize/datamart_materialize/d3m.py
+++ lib_materialize/datamart_materialize/d3m.py
@@ -1,7 +1,12 @@
+import csv
 import json
+import logging
 import os
 
 from . import types
+
+
+logger = logging.getLogger(__name__)
 
 
 STRUCTURAL_TYPE_MAP = {
@@ -87,7 +92,7 @@ class D3mWriter(object):
     of the schema to generate, between ``3.2.0`` and ``4.0.0``.
     """
     needs_metadata = True
-    default_options = {'version': DEFAULT_VERSION}
+    default_options = {'version': DEFAULT_VERSION, 'need_d3mindex': False}
 
     @classmethod
     def _get_opt(cls, options, key):
@@ -98,6 +103,7 @@ class D3mWriter(object):
 
     def __init__(self, dataset_id, destination, metadata, format_options=None):
         version = self._get_opt(format_options, 'version')
+        self.need_d3mindex = self._get_opt(format_options, 'need_d3mindex')
         if format_options:
             raise ValueError(
                 "Invalid format option %r" % (next(iter(format_options)),)
@@ -121,4 +127,30 @@ class D3mWriter(object):
                     mode, **kwargs)
 
     def finish(self):
+        if self.need_d3mindex:
+            overwrite = False
+            tables = os.path.join(self.destination, 'tables')
+            data_path = os.path.join(tables, 'learningData.csv')
+            tmp_path = os.path.join(tables, 'learningData.index.csv')
+            with open(data_path) as f_in:
+                reader = iter(csv.reader(f_in))
+
+                try:
+                    columns = next(reader)
+                except StopIteration:
+                    return None
+                if 'd3mIndex' not in columns:
+                    logger.info("No 'd3mIndex' column, generating one")
+                    overwrite = True
+                    with open(
+                        tmp_path,
+                        'w',
+                    ) as f_out:
+                        writer = csv.writer(f_out)
+                        writer.writerow(['d3mIndex'] + columns)
+                        for idx, row in enumerate(reader):
+                            writer.writerow([idx] + row)
+            if overwrite:
+                os.rename(tmp_path, data_path)
+
         return None
