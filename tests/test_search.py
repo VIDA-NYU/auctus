e3df73ea5bb0df98374dc34657ebb5a8dfe61bd9
Remi Rampin <r@remirampin.com>
2020-04-06 16:53:43 -0400

    Add test for query parsing

---
+++ tests/test_search.py
@@ -0,0 +1,117 @@
+import unittest
+
+from query.search import parse_query
+
+
+class TestSearch(unittest.TestCase):
+    def test_simple(self):
+        main, sup, vars = parse_query({
+            'keywords': ['green', 'taxi'],
+            'source': 'gov',
+        })
+        self.assertEqual(
+            main,
+            [
+                {
+                    'bool': {
+                        'should': [
+                            {
+                                'match': {
+                                    'description': {
+                                        'query': 'green taxi',
+                                        'operator': 'and',
+                                    },
+                                },
+                            },
+                            {
+                                'match': {
+                                    'name': {
+                                        'query': 'green taxi',
+                                        'operator': 'and',
+                                    },
+                                },
+                            },
+                            {
+                                'nested': {
+                                    'path': 'columns',
+                                    'query': {
+                                        'match': {
+                                            'columns.name': {
+                                                'query': 'green taxi',
+                                                'operator': 'and',
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        ],
+                        'minimum_should_match': 1,
+                    },
+                },
+                {
+                    'bool': {
+                        'filter': [
+                            {
+                                'terms': {
+                                    'source': 'gov',
+                                },
+                            },
+                        ],
+                    },
+                },
+            ],
+        )
+        self.assertEqual(
+            sup,
+            [
+                {
+                    'bool': {
+                        'should': [
+                            {
+                                'filter': {
+                                    'match': {
+                                        'dataset_description': {
+                                            'query': 'green taxi',
+                                            'operator': 'and',
+                                        },
+                                    },
+                                },
+                            },
+                            {
+                                'filter': {
+                                    'match': {
+                                        'dataset_name': {
+                                            'query': 'green taxi',
+                                            'operator': 'and',
+                                        },
+                                    },
+                                },
+                            },
+                            {
+                                'filter': {
+                                    'match': {
+                                        'name': {
+                                            'query': 'green taxi',
+                                            'operator': 'and',
+                                        },
+                                    },
+                                },
+                            },
+                        ],
+                        'minimum_should_match': 1,
+                    },
+                },
+                {
+                    'bool': {
+                        'filter': [
+                            {
+                                'terms': {
+                                    'dataset_source': 'gov',
+                                },
+                            },
+                        ],
+                    },
+                },
+            ],
+        )
+        self.assertEqual(vars, [])
