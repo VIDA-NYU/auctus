637ceb80815a3e0088a757015420b818bdc1b6f2
Remi Rampin <r@remirampin.com>
2020-04-07 14:30:06 -0400

    Test coverage queries

--- tests/test_search.py
+++ tests/test_search.py
@@ -129,3 +129,141 @@ class TestSearch(unittest.TestCase):
             ],
         )
         self.assertEqual(vars, [])
+
+    def test_ranges(self):
+        main, sup_funcs, sup_filters, vars = parse_query({
+            'keywords': ['green', 'taxi'],
+            'source': 'gov',
+            'variables': [
+                {
+                    'type': 'temporal_variable',
+                    'start': '2019-01-01',
+                    'end': '2019-12-31',
+                },
+                {
+                    'type': 'geospatial_variable',
+                    'latitude1': 45.4,
+                    'latitude2': 50.6,
+                    'longitude1': -73.2,
+                    'longitude2': -75.8,
+                },
+            ],
+        })
+        self.assertEqual(
+            main,
+            [
+                {
+                    'bool': {
+                        'should': [
+                            {
+                                'match': {
+                                    'id': {
+                                        'query': 'green taxi',
+                                        'operator': 'and',
+                                    },
+                                },
+                            },
+                            {
+                                'match': {
+                                    'description': {
+                                        'query': 'green taxi',
+                                        'operator': 'and',
+                                    },
+                                },
+                            },
+                            {
+                                'match': {
+                                    'name': {
+                                        'query': 'green taxi',
+                                        'operator': 'and',
+                                    },
+                                },
+                            },
+                            {
+                                'nested': {
+                                    'path': 'columns',
+                                    'query': {
+                                        'match': {
+                                            'columns.name': {
+                                                'query': 'green taxi',
+                                                'operator': 'and',
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        ],
+                        'minimum_should_match': 1,
+                    },
+                },
+                {
+                    'bool': {
+                        'filter': [
+                            {
+                                'terms': {
+                                    'source': 'gov',
+                                },
+                            },
+                        ],
+                    },
+                },
+                [
+                    {
+                        'bool': {
+                            'must': [
+                                {
+                                    'nested': {
+                                        'path': 'columns',
+                                        'query': {
+                                            'term': {'columns.semantic_types': 'http://schema.org/DateTime'},
+                                        },
+                                    },
+                                },
+                                {
+                                    'nested': {
+                                        'path': 'columns.coverage',
+                                        'query': {
+                                            'range': {
+                                                'columns.coverage.range': {
+                                                    'gte': 1546300800.0,
+                                                    'lte': 1577750400.0,
+                                                    'relation': 'intersects'
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        'bool': {
+                            'must': [
+                                {
+                                    'nested': {
+                                        'path': 'spatial_coverage.ranges',
+                                        'query': {
+                                            'bool': {
+                                                'filter': {
+                                                    'geo_shape': {
+                                                        'spatial_coverage.ranges.range': {
+                                                            'shape': {
+                                                                'type': 'envelope',
+                                                                'coordinates':
+                                                                    [[-75.8, 50.6],
+                                                                     [-73.2, 45.4]]
+                                                            },
+                                                            'relation': 'intersects'
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                ],
+            ],
+        )
