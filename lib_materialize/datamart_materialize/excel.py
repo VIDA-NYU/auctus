bc0321a8dd9e72f85228852eea5103949673b444
Remi Rampin <r@remirampin.com>
2020-05-07 12:46:59 -0400

    Factor the Excel converter

--- lib_materialize/datamart_materialize/excel.py
+++ lib_materialize/datamart_materialize/excel.py
@@ -1,7 +1,7 @@
 import csv
-import os
-import tempfile
 import xlrd
+
+from .utils import SimpleConverter
 
 
 def xls_to_csv(source_filename, dest_fileobj):
@@ -16,53 +16,7 @@ def xls_to_csv(source_filename, dest_fileobj):
         writer.writerow(sheet.row_values(row_num))
 
 
-class _ExcelProxy(object):
-    def __init__(self, writer, name, temp_file, fp):
-        self._writer = writer
-        self._name = name
-        self._temp_file = temp_file
-        self._fp = fp
-
-    def close(self):
-        self._fp.close()
-        self._convert()
-
-    def _convert(self):
-        # Read back the XLS file we wrote, and write CSV via the writer
-        with self._writer.open_file('w', self._name, newline='') as dst:
-            xls_to_csv(self._temp_file, dst)
-
-    # Those methods forward to the actual file object
-
-    def write(self, buffer):
-        return self._fp.write(buffer)
-
-    def flush(self):
-        self._fp.flush()
-
-    def __enter__(self):
-        self._fp.__enter__()
-        return self
-
-    def __exit__(self, exc, value, tb):
-        self._fp.__exit__(exc, value, tb)
-        self._convert()
-
-
-class ExcelConverter(object):
+class ExcelConverter(SimpleConverter):
     """Adapter converting Excel files to CSV.
     """
-    def __init__(self, writer):
-        self.writer = writer
-        self.dir = tempfile.TemporaryDirectory(prefix='datamart_excel_')
-
-    def open_file(self, mode='wb', name=None, **kwargs):
-        temp_file = os.path.join(self.dir.name, 'file.xls')
-
-        # Return a proxy that will write to the destination when closed
-        fp = open(temp_file, mode, **kwargs)
-        return _ExcelProxy(self.writer, name, temp_file, fp)
-
-    def finish(self):
-        self.dir.cleanup()
-        self.dir = None
+    transform = staticmethod(xls_to_csv)
