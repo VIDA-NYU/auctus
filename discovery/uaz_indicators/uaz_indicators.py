45f39dad288a6b3ea8b6a32d65741e2f4d4f62a3
Remi Rampin <r@remirampin.com>
2020-05-19 16:42:55 -0400

    Make datasets with multiple variables, with SQL
    Problem: SQLite3 doesn't support FULL OUTER JOIN.

--- discovery/uaz_indicators/uaz_indicators.py
+++ discovery/uaz_indicators/uaz_indicators.py
@@ -1,5 +1,6 @@
 import asyncio
 import csv
+import textwrap
 import uuid
 from datetime import datetime, timedelta
 import elasticsearch.helpers
@@ -85,47 +86,97 @@ class UazIndicatorsDiscoverer(Discoverer):
 
     def discover_indicators(self, filename):
         conn = sqlite3.connect(filename)
-        indicators = conn.execute('''\
-            SELECT DISTINCT Source, Unit, Variable FROM indicator;
+        indicators = iter(conn.execute('''\
+            SELECT DISTINCT Variable, Unit
+            FROM indicator
+            ORDER BY Variable;
+        '''))
+        current_prefix = None
+        variables = []
+        for variable_name, unit in indicators:
+            idx = variable_name.find(',')
+            if idx == -1:
+                prefix = variable_name
+            else:
+                prefix = variable_name[:idx]
+
+            # If we're still on the same prefix, add it to the variables
+            if current_prefix is None:
+                current_prefix = prefix
+            elif prefix != current_prefix:
+                self.make_indicator_dataset(conn, current_prefix, variables)
+                current_prefix = prefix
+                variables = []
+
+            variables.append((variable_name, unit))
+
+        if variables:
+            self.make_indicator_dataset(conn, current_prefix, variables)
+
+    def make_indicator_dataset(self, conn, variable_prefix, variables):
+        name = variable_prefix
+        dataset_id = uuid.uuid5(self.NAMESPACE, variable_prefix).hex
+        logger.info(
+            "Making indicator dataset %r variables=%r",
+            variable_prefix, variables,
+        )
+
+        # Build a query joining all the columns
+        columns = ', '.join(
+            f'i{nb}.Value AS value{nb}'
+            for nb in range(len(variables))
+        )
+        joins = '\n'.join(
+            textwrap.dedent(f'''\
+            FULL OUTER JOIN indicator i{nb}
+                ON
+                    i0.Country = i{nb}.Country AND
+                    i0.State = i{nb}.State AND
+                    i0.County = i{nb}.County AND
+                    i0.Year = i{nb}.Year AND
+                    i0.Month = i{nb}.Month AND
+                    i{nb}.Variable = :variable{nb} AND i{nb}.Unit = :unit{nb}
+            ''')
+            for nb in range(1, len(variables))
+        )
+        params = {}
+        for nb, (variable_name, unit) in enumerate(variables):
+            params[f'variable{nb}'] = variable_name
+            params[f'unit{nb}'] = unit
+        query = textwrap.dedent(f'''\
+            SELECT
+                i0.Country AS Country, i0.State AS State,
+                i0.County AS County, i0.Year AS Year, i0.Month AS Month,
+                {columns}
+            FROM indicator i0
+            {joins}
+            WHERE
+                i0.Variable = :variable0 AND i0.Unit = :unit0
+            GROUP BY Country, State, County, Year, Month;
         ''')
-        for source, unit, variable_name in indicators:
-            name = "%s (%s) from %s" % (variable_name, unit, source)
-            dataset_id = uuid.uuid5(self.NAMESPACE, name).hex
-            cursor = conn.execute(
-                '''\
-                    SELECT Country, State, County, Year, Month, Value
-                    FROM indicator
-                    WHERE Source=? AND Unit=? AND Variable=?;
-                ''',
-                (source, unit, variable_name),
-            )
-            with self.write_to_shared_storage(dataset_id) as tmp:
-                with open(os.path.join(tmp, 'main.csv'), 'w') as fp:
-                    writer = csv.writer(fp)
+        logger.warning("Running query:\n%s\nparams:\n%r", query, params)
+        cursor = conn.execute(query, params)
+        with self.write_to_shared_storage(dataset_id) as tmp:
+            with open(os.path.join(tmp, 'main.csv'), 'w') as fp:
+                writer = csv.writer(fp)
+                writer.writerow(
+                    ['Country', 'State', 'County', 'Year', 'Month'] +
+                    [
+                        "%s (%s)" % (variable, unit)
+                        for variable, unit in variables
+                    ],
+                )
+                for row in cursor:
                     writer.writerow([
-                        'Country', 'State', 'County',
-                        'Year', 'Month',
-                        'Value',
+                        e if e is not None else ''
+                        for e in row
                     ])
-                    for row in cursor:
-                        writer.writerow([
-                            e if e is not None else ''
-                            for e in row
-                        ])
-            self.record_dataset(
-                dict(
-                    uaz_indicators_source=source,
-                    uaz_indicators_variable=variable_name,
-                    uaz_indicators_unit=unit,
-                ),
-                dict(
-                    name=name,
-                    unit=unit,
-                    source='%s (UAZ)' % source,
-                ),
-                dataset_id=dataset_id,
-            )
-            cursor.close()
+        self.record_dataset(
+            dict(uaz_indicators_variable_prefix=variable_prefix),
+            dict(name=name),
+            dataset_id=dataset_id,
+        )
+        cursor.close()
 
     def discover_dssat(self, filename):
         pass  # TODO
