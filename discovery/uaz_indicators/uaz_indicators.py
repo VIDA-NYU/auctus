ec3d7ee13900e86dcbf9773759282f05f31e58a7
Remi Rampin <r@remirampin.com>
2020-05-19 17:36:49 -0400

    Make datasets with multiple variables, with Pandas

--- discovery/uaz_indicators/uaz_indicators.py
+++ discovery/uaz_indicators/uaz_indicators.py
@@ -1,10 +1,10 @@
 import asyncio
-import csv
 import uuid
 from datetime import datetime, timedelta
 import elasticsearch.helpers
 import logging
 import os
+import pandas
 import requests
 import sqlite3
 import tempfile
@@ -85,47 +85,88 @@ class UazIndicatorsDiscoverer(Discoverer):
 
     def discover_indicators(self, filename):
         conn = sqlite3.connect(filename)
-        indicators = conn.execute('''\
-            SELECT DISTINCT Source, Unit, Variable FROM indicator;
-        ''')
-        for source, unit, variable_name in indicators:
-            name = "%s (%s) from %s" % (variable_name, unit, source)
-            dataset_id = uuid.uuid5(self.NAMESPACE, name).hex
-            cursor = conn.execute(
+        indicators = iter(conn.execute('''\
+            SELECT DISTINCT Variable, Unit
+            FROM indicator
+            ORDER BY Variable;
+        '''))
+        current_prefix = None
+        variables = []
+        for variable_name, unit in indicators:
+            idx = variable_name.find(',')
+            if idx == -1:
+                prefix = variable_name
+            else:
+                prefix = variable_name[:idx]
+
+            # If we're still on the same prefix, add it to the variables
+            if current_prefix is None:
+                current_prefix = prefix
+            elif prefix != current_prefix:
+                self.make_indicator_dataset(conn, current_prefix, variables)
+                current_prefix = prefix
+                variables = []
+
+            variables.append((variable_name, unit))
+
+        if variables:
+            self.make_indicator_dataset(conn, current_prefix, variables)
+
+    def make_indicator_dataset(self, conn, variable_prefix, variables):
+        name = variable_prefix
+        dataset_id = uuid.uuid5(self.NAMESPACE, variable_prefix).hex
+        logger.info(
+            "Making indicator dataset %r variables=%r",
+            variable_prefix, variables,
+        )
+
+        # Read data using Pandas
+        dataframes = []
+        for variable_name, unit in variables:
+            dataframes.append(pandas.read_sql_query(
                 '''\
-                    SELECT Country, State, County, Year, Month, Value
-                    FROM indicator
-                    WHERE Source=? AND Unit=? AND Variable=?;
+                SELECT
+                    Country, State, County, Year, Month,
+                    Variable, Unit,
+                    Value
+                FROM indicator
+                WHERE Variable = ? AND Unit = ?;
                 ''',
-                (source, unit, variable_name),
-            )
-            with self.write_to_shared_storage(dataset_id) as tmp:
-                with open(os.path.join(tmp, 'main.csv'), 'w') as fp:
-                    writer = csv.writer(fp)
-                    writer.writerow([
-                        'Country', 'State', 'County',
-                        'Year', 'Month',
-                        'Value',
-                    ])
-                    for row in cursor:
-                        writer.writerow([
-                            e if e is not None else ''
-                            for e in row
-                        ])
-            self.record_dataset(
-                dict(
-                    uaz_indicators_source=source,
-                    uaz_indicators_variable=variable_name,
-                    uaz_indicators_unit=unit,
-                ),
-                dict(
-                    name=name,
-                    unit=unit,
-                    source='%s (UAZ)' % source,
-                ),
-                dataset_id=dataset_id,
-            )
-            cursor.close()
+                conn,
+                params=(variable_name, unit),
+            ))
+        df = pandas.concat(dataframes, axis=0)
+
+        # Make a single column 'name (unit)'
+        df['Variable'] = df.apply(
+            lambda row: "%s (%s)" % (row['Variable'], row['Unit']),
+            axis=1,
+        )
+        df.drop(['Unit'], axis=1, inplace=True)
+
+        # Pivot the concatenated data, put variables in different columns
+        df.index.name = '_dummy_index'
+        df = df.set_index(
+            [
+                'Country', 'State', 'County', 'Year', 'Month',
+                'Variable',
+            ],
+            append=True,
+        )
+        df = df.unstack(level=[-1])
+        df.columns = df.columns.get_level_values(1)
+        df.columns.name = None
+        df = df.reset_index()
+        df = df.drop(['_dummy_index'], axis=1)
+
+        with self.write_to_shared_storage(dataset_id) as tmp:
+            df.to_csv(os.path.join(tmp, 'main.csv'), index=False)
+
+        self.record_dataset(
+            dict(uaz_indicators_variable_prefix=variable_prefix),
+            dict(name=name),
+            dataset_id=dataset_id,
+        )
 
     def discover_dssat(self, filename):
         pass  # TODO
