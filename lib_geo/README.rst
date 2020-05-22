e74e7546ccd718a459db54774f7319c437f38b3e
Remi Rampin <r@remirampin.com>
2020-05-22 12:41:17 -0400

    Start a lib_geo

---
+++ lib_geo/README.rst
@@ -0,0 +1,10 @@
+Datamart geospatial data
+========================
+
+This package contains geospatial data and associated facilities to resolve administrative areas. It is used by Auctus, NYU's Datamart system, to resolved named areas during profiling.
+
+See also:
+
+* `The datamart-profiler library, used to profile datasets for search <https://pypi.org/project/datamart-profiler/>`__
+* `Auctus, NYU's Datamart system <https://auctus.vida-nyu.org/>`__
+* `Our project on GitLab <https://gitlab.com/ViDA-NYU/datamart/datamart>`__
---
+++ lib_geo/setup.py
@@ -0,0 +1,35 @@
+import io
+import os
+from setuptools import setup
+
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+with io.open('README.rst', encoding='utf-8') as fp:
+    description = fp.read()
+setup(name='datamart-geo',
+      version='0.1',
+      packages=['datamart_geo'],
+      description="Datamart geographical data",
+      author="Remi Rampin",
+      author_email='remi.rampin@nyu.edu',
+      maintainer="Remi Rampin",
+      maintainer_email='remi.rampin@nyu.edu',
+      url='https://gitlab.com/ViDA-NYU/datamart/datamart',
+      project_urls={
+          'Homepage': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Source': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Tracker': 'https://gitlab.com/ViDA-NYU/datamart/datamart/issues',
+      },
+      long_description=description,
+      license='BSD-3-Clause',
+      keywords=['datamart'],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: Free for non-commercial use',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Database'])
