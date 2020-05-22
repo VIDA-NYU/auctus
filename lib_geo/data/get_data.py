ce5dbf70787d4a5197de7ae65a8879c9fa1da981
Remi Rampin <r@remirampin.com>
2020-05-22 13:09:40 -0400

    Factor

--- lib_geo/data/get_data.py
+++ lib_geo/data/get_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import csv
+import functools
 import json
 import logging
 import os
@@ -31,14 +32,48 @@ def sparql_query(query):
     return results
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-    os.chdir(os.path.dirname(__file__) or '.')
+def makes_file(name):
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped():
+            if os.path.exists(name):
+                logger.info("Skipping: %s", func.__doc__.splitlines()[0])
+                return
+            logger.info("Running: %s", func.__doc__.splitlines()[0])
+            try:
+                with open(name, 'w', newline='', encoding='utf-8') as fp:
+                    writer = csv.writer(fp)
+                    func(writer)
+            except BaseException:
+                os.remove(name)
+                raise
+        return wrapped
+    return wrapper
 
-    # Get all countries with their geometry
-    # P31 = instance of
-    # Q6256 = country
-    countries = sparql_query(
+
+def literal(item):
+    assert item['type'] == 'literal'
+    return item['value']
+
+
+def q_entity_uri(item):
+    assert item['type'] == 'uri'
+    prefix = 'http://www.wikidata.org/entity/'
+    value = item['value']
+    assert value.startswith(prefix)
+    return value[len(prefix):]
+
+
+def uri(item):
+    assert item['type'] == 'uri'
+    return item['value']
+
+
+@makes_file('geoshapes.csv')
+def geoshapes(writer):
+    """Get all countries with their geometry.
+    """
+    rows = sparql_query(
         'SELECT ?item ?shape\n'
         'WHERE\n'
         '{\n'
@@ -47,33 +82,33 @@ def main():
         '  MINUS{ ?item wdt:P31 wd:Q3024240. }\n'  # not "historical country"
         '}\n'
     )
-    with open('geoshapes.csv', 'w', newline='', encoding='utf-8') as fp:
-        writer = csv.writer(fp)
-        writer.writerow(['country', 'geoshape URL', 'geoshape'])
-        for country in countries:
-            assert country['item']['type'] == 'uri'
-            prefix = 'http://www.wikidata.org/entity/'
-            value = country['item']['value']
-            assert value.startswith(prefix)
-            value = value[len(prefix):]
 
-            assert country['shape']['type'] == 'uri'
-            shape_uri = country['shape']['value']
+    writer.writerow(['country', 'geoshape URL', 'geoshape'])
+    for row in rows:
+        value = q_entity_uri(row['item'])
+        shape_uri = uri(row['shape'])
 
-            try:
-                logger.info("Getting geoshape %s", shape_uri)
-                shape_resp = requests.get(shape_uri)
-                shape_resp.raise_for_status()
-                shape = json.dumps(
-                    shape_resp.json(),
-                    # Compact
-                    sort_keys=True, indent=None, separators=(',', ':'),
-                )
-            except requests.exceptions.HTTPError as e:
-                logger.error("Error getting geoshape: %s", e)
-                shape = None
+        try:
+            logger.info("Getting geoshape %s", shape_uri)
+            shape_resp = requests.get(shape_uri)
+            shape_resp.raise_for_status()
+            shape = json.dumps(
+                shape_resp.json(),
+                # Compact
+                sort_keys=True, indent=None, separators=(',', ':'),
+            )
+        except requests.exceptions.HTTPError as e:
+            logger.error("Error getting geoshape: %s", e)
+            shape = None
 
-            writer.writerow([value, shape_uri, shape])
+        writer.writerow([value, shape_uri, shape])
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    os.chdir(os.path.dirname(__file__) or '.')
+
+    geoshapes()
 
 
 if __name__ == '__main__':
