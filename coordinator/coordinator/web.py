6c243c15cece9f5bb2e062818e13ffc29cf49e39
Remi Rampin <r@remirampin.com>
2020-06-02 17:06:06 -0400

    Fix logging error

--- coordinator/coordinator/web.py
+++ coordinator/coordinator/web.py
@@ -151,7 +151,7 @@ def main():
     setup_logging()
     debug = os.environ.get('DEBUG') not in (None, '', 'no', 'off', 'false')
     prometheus_client.start_http_server(8000)
-    logger.info("Startup: coordinator %s%s", os.environ['DATAMART_VERSION'])
+    logger.info("Startup: coordinator %s", os.environ['DATAMART_VERSION'])
     if debug:
         logger.error("Debug mode is ON")
 
