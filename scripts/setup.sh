c9d1b242cbe0014c8706bfd7ceacec71f6270097
Remi Rampin <r@remirampin.com>
2020-06-02 11:42:02 -0400

    Don't run containers as root

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,6 +11,8 @@ sudo(){
 }
 
 # Set up volume permissions
+mkdir -p volumes/datasets && sudo chown -R 998 volumes/datasets
+mkdir -p volumes/cache && sudo chown -R 998 volumes/cache
 mkdir -p volumes/prometheus && sudo chown -R 65534:65534 volumes/prometheus
 mkdir -p volumes/elasticsearch && sudo chown -R 1000:0 volumes/elasticsearch
 mkdir -p volumes/grafana && sudo chown -R 472:472 volumes/grafana
