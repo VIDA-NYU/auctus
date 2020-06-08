d7e9e23b7f0c456e27aa20fb3b3324812709a8cc
Remi Rampin <r@remirampin.com>
2020-06-08 11:14:16 -0400

    Update docker_export_all.sh script for no-root

--- scripts/docker_export_all.sh
+++ scripts/docker_export_all.sh
@@ -5,4 +5,6 @@ if [ -z "$1" ]; then
     echo "Missing argument" >&2
     exit 1
 fi
+mkdir "$1"
+chown 998 "$1"
 docker run --rm --network ${PROJ}_default -v $PWD/scripts:/scripts -v "$1:/index" -w /index -e ELASTICSEARCH_HOSTS=elasticsearch:9200 ${PROJ}_coordinator python /scripts/export_all.py
