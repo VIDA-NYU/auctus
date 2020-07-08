7a320219d6889c3d3a20d586c817eb12de5aef18
Remi Rampin <r@remirampin.com>
2020-07-08 13:57:06 -0400

    Update load_env.sh to quote values

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sed 's/^/export /' .env >.env.sh && \
-. ".env.sh" >/dev/null && \
-rm ".env.sh" && \
+while read line; do
+  if [ "$line" != "" -a "${line:0:1}" != "#" ]; then
+    var="$(echo "$line" | sed 's/^\([^=]\+\)=\(.*\)$/\1/')"
+    val="$(echo "$line" | sed 's/^\([^=]\+\)=\(.*\)$/\2/')"
+    eval "export $var=${val@Q}"
+  fi
+done <.env
 export DATAMART_VERSION=$(git describe)
 export DATAMART_GEO_DATA="$(pwd)/lib_geo/data"
