59295521b4f264ffc9f4f9e2433fc08e0cc18e28
Aline Bessa <alibezz@Alines-MBP.home>
2020-07-10 18:36:46 -0400

    fixing test issue

--- tests/test_integ.py
+++ tests/test_integ.py
@@ -247,6 +247,7 @@ class TestProfiler(DataTestCase):
 
         self.assertJson(actual, expected)
 
+        
 class TestProfileQuery(DatamartTest):
     def check_result(self, response, metadata, token):
         # Some fields like 'name', 'description' won't be there
