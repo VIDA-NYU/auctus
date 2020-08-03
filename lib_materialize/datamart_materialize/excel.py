19d3e86e4e893157dfbb6dfcdff447ae74e3f80c
Remi Rampin <r@remirampin.com>
2020-08-03 13:53:55 -0400

    Decode dates from Excel files

--- lib_materialize/datamart_materialize/excel.py
+++ lib_materialize/datamart_materialize/excel.py
@@ -1,11 +1,13 @@
 import csv
 import xlrd
+import xlrd.sheet
 
 from .utils import SimpleConverter
 
 
 def xls_to_csv(source_filename, dest_fileobj):
     workbook = xlrd.open_workbook(source_filename)
+    datemode = workbook.datemode
     sheets = workbook.sheets()
     if len(sheets) != 1:
         raise ValueError("Excel workbook has %d sheets" % len(sheets))
@@ -13,7 +15,17 @@ def xls_to_csv(source_filename, dest_fileobj):
 
     writer = csv.writer(dest_fileobj)
     for row_num in range(sheet.nrows):
-        writer.writerow(sheet.row_values(row_num))
+        values = sheet.row_values(row_num)
+
+        # Decode dates into ISO-8601 strings
+        for col_num, cell_type in enumerate(sheet.row_types(row_num)):
+            if cell_type == xlrd.sheet.XL_CELL_DATE:
+                values[col_num] = xlrd.xldate_as_datetime(
+                    values[col_num],
+                    datemode,
+                ).isoformat()
+
+        writer.writerow(values)
 
 
 class ExcelConverter(SimpleConverter):
