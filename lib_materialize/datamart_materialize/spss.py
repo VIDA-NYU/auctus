54cd14bb3caad4a68f0c723de9cf36cc38d47389
Remi Rampin <r@remirampin.com>
2020-08-04 19:09:20 -0400

    Don't turn integers into floats from SPSS files

--- lib_materialize/datamart_materialize/spss.py
+++ lib_materialize/datamart_materialize/spss.py
@@ -5,7 +5,12 @@ from datamart_materialize.utils import SimpleConverter
 
 def spss_to_csv(source_filename, dest_fileobj):
     df, meta = pyreadstat.read_sav(source_filename)
-    df.to_csv(dest_fileobj, index=False, line_terminator='\r\n')
+    df.to_csv(
+        dest_fileobj,
+        float_format='%g',
+        index=False,
+        line_terminator='\r\n',
+    )
 
 
 class SpssConverter(SimpleConverter):
