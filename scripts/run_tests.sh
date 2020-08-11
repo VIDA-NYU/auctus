ce91c0e37a30d85c80f394266d1a9fc36a3d2ce5
Remi Rampin <r@remirampin.com>
2020-08-10 22:02:27 -0400

    Raise profiling timeout in run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -29,7 +29,7 @@ docker-compose up -d --force-recreate test-discoverer
 
 # Wait for profiling to end
 (set +x
-slept=0; while [ $slept -le 40 -a $(curl -s -o /dev/null -w "%{http_code}" http://localhost:9200/datamart/_doc/datamart.test.basic) != 200 ]; do sleep 1; slept=$((slept + 1)); done
+slept=0; while [ $slept -le 180 -a $(curl -s -o /dev/null -w "%{http_code}" http://localhost:9200/datamart/_doc/datamart.test.basic) != 200 ]; do sleep 1; slept=$((slept + 1)); done
 if [ $slept -gt 40 ]; then
   echo "Profiling didn't end after ${slept}s"
   exit 1
