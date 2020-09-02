8bda44166316ca79882364726ed870ceb7cdd5e0
Remi Rampin <r@remirampin.com>
2020-09-02 00:08:39 -0400

    Don't compute 3-grams for Levenshtein distance

--- apiserver/apiserver/search/union.py
+++ apiserver/apiserver/search/union.py
@@ -19,17 +19,7 @@ def compute_levenshtein_sim(str1, str2):
     if str1 in str2 or str2 in str1:
         return 1
 
-    if len(str1) < 3:
-        str1_set = [str1]
-    else:
-        str1_set = [str1[i:i + 3] for i in range(len(str1) - 2)]
-
-    if len(str2) < 3:
-        str2_set = [str2]
-    else:
-        str2_set = [str2[i:i + 3] for i in range(len(str2) - 2)]
-
-    return 1 - distance.nlevenshtein(str1_set, str2_set, method=2)
+    return 1 - distance.nlevenshtein(str1, str2, method=2)
 
 
 def get_columns_by_type(data_profile, filter_=()):
