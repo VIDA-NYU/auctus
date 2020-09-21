0fcaf242a32978eaf545dd5ba9ca03233f66a797
Remi Rampin <r@remirampin.com>
2020-09-21 11:41:11 -0400

    Add support for separators other than tab & commas

--- lib_materialize/datamart_materialize/tsv.py
+++ lib_materialize/datamart_materialize/tsv.py
@@ -3,15 +3,20 @@ import csv
 from datamart_materialize.utils import SimpleConverter
 
 
-def tsv_to_csv(source_filename, dest_fileobj):
+def tsv_to_csv(source_filename, dest_fileobj, separator='\t'):
     with open(source_filename, 'r') as src_fp:
-        src = csv.reader(src_fp, delimiter='\t')
+        src = csv.reader(src_fp, delimiter=separator)
         dst = csv.writer(dest_fileobj)
         for line in src:
             dst.writerow(line)
 
 
 class TsvConverter(SimpleConverter):
-    """Adapter converting a TSV file to CSV.
+    """Adapter converting a TSV or other separated file to CSV.
     """
-    transform = staticmethod(tsv_to_csv)
+    def __init__(self, writer, separator='\t'):
+        self.separator = separator
+        super(TsvConverter, self).__init__(writer)
+
+    def transform(self, source_filename, dest_fileobj):
+        tsv_to_csv(source_filename, dest_fileobj, separator=self.separator)
