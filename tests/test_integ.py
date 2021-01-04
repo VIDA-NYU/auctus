7d9083b8f06a0b2f570925dfb58e5c105637ebf8
Remi Rampin <r@remirampin.com>
2021-01-04 14:42:20 -0500

    Add test for XLS

--- tests/test_integ.py
+++ tests/test_integ.py
@@ -151,6 +151,7 @@ class TestProfiler(DatamartTest):
             'datamart.test.dates_pivoted': dates_pivoted_metadata,
             'datamart.test.years_pivoted': years_pivoted_metadata,
             'datamart.test.excel': other_formats_metadata('xlsx'),
+            'datamart.test.excel97': other_formats_metadata('xls'),
             'datamart.test.spss': other_formats_metadata('spss'),
             'datamart.test.stata114': other_formats_metadata('stata'),
             'datamart.test.stata118': other_formats_metadata('stata'),
