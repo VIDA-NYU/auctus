fe332fc2b452fe43c9b2842b7d176179261137b6
Remi Rampin <remi@rampin.org>
2021-04-06 18:04:23 -0400

    Add build script for Kubernetes

---
+++ scripts/docker_build_push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eux
+
+cd "$(dirname "$0")/.."
+
+CONTAINERS="frontend apiserver coordinator cache-cleaner profiler socrata zenodo worldbank uaz-indicators"
+VERSION=$(git describe)
+
+# Build
+docker-compose build --build-arg version=$VERSION $CONTAINERS
+
+# Push
+for i in $CONTAINERS; do
+    docker tag auctus_$i registry.gitlab.com/vida-nyu/auctus/auctus/$i:$VERSION
+    docker push registry.gitlab.com/vida-nyu/auctus/auctus/$i:$VERSION
+done
