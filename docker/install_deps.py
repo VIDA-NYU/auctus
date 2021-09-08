373f2cb06ecfadc2303a04e2dcc72505db0091af
Remi Rampin <remi@rampin.org>
2021-09-08 13:35:19 -0400

    Have install_deps.py load poetry.lock TOML

--- docker/install_deps.py
+++ docker/install_deps.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Install dependencies from poetry.lock.json
+"""Install dependencies from poetry.lock
 
 This scripts is used as part of the Docker build to install all dependencies as
 an initial step before building the images. This makes caching efficient,
@@ -9,19 +9,16 @@ allowing for faster builds that work offline.
 It means all images have all dependencies installed, but thanks to
 de-duplication, this generally uses less space if all images exist on the same
 machine.
-
-Poetry actually uses TOML, which Python can't read directly, so we have an
-extra step to convert from TOML to JSON before this is run.
 """
 
-import json
 import subprocess
 import sys
+import toml
 
 
 def main():
     with open(sys.argv[1]) as fp:
-        lockfile = json.load(fp)
+        lockfile = toml.load(fp)
 
     packages = []
 
