01e880dc5ee93a3d2413952d6fc71217524a6a64
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 13:18:12 +0000

    Adding tarfile member sanitization to extractall()

--- discovery/isi/isi_discovery.py
+++ discovery/isi/isi_discovery.py
@@ -72,7 +72,26 @@ class IsiDiscoverer(Discoverer):
                     tempfile.TemporaryDirectory()
                 )
                 with tarfile.open(tarball.name, 'r') as tar:
-                    tar.extractall(folder)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, folder)
                 folder = os.path.join(folder, 'datamart-dump')
 
             seen = set()
