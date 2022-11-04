c5076fa31c4647e7490c71ff2e4cc42dd85e61f1
Remi Rampin <remi@rampin.org>
2022-11-04 18:02:26 -0400

    Unlock lib_profiler's scikit-learn dependency

--- lib_profiler/setup.py
+++ lib_profiler/setup.py
@@ -12,7 +12,7 @@ req = [
     'pandas',
     'prometheus_client',
     'python-dateutil',
-    'scikit-learn>=0.22,<0.24',
+    'scikit-learn',
     'regex',
     'requests',
     'datamart-geo>=0.2.3,<0.4',
