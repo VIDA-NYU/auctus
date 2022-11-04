c0d238eeb78eeb918fdcad90f985a78153a874b6
Remi Rampin <remi@rampin.org>
2022-11-04 18:28:31 -0400

    Bump lib_profiler to 0.10.1

--- lib_profiler/datamart_profiler/__init__.py
+++ lib_profiler/datamart_profiler/__init__.py
@@ -2,7 +2,7 @@ from .core import count_rows_to_skip, process_dataset
 from .temporal import parse_date
 
 
-__version__ = '0.10'
+__version__ = '0.10.1'
 
 
 __all__ = ['count_rows_to_skip', 'process_dataset', 'parse_date']
